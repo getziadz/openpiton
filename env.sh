c011ecfb71ba9068a294fd7473e43e22c385b82e
Georgios <gt5@princeton.edu>
2019-01-15 17:27:41 -0500

    add env.sh script

---
+++ env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+__TMP=$(dirname "$(readlink -f \"$0\")")
+
+export PITON_ROOT=${__TMP}
+echo "PITON_ROOT=${PITON_ROOT}"
+
+. ${PITON_ROOT}/piton/piton_settings.bash
+
+function urg() { command urg -full64 "$@"; }; export -f urg
+function vcs() { command vcs -full64 "$@"; }; export -f vcs
+
+
+if [ -x `command -v iverilog` ];
+then
+    echo "IVERILOG in PATH"
+    ICARUS_HOME=`command -v iverilog`
+    ICARUS_HOME=$(dirname ${ICARUS_HOME})
+    ICARUS_HOME=$(dirname ${ICARUS_HOME})
+elif [ "${HOSTNAME}" == "della2.princeton.edu" ] || 
+   [ "${HOSTNAME}" == "della2.princeton.edu" ];
+then
+    echo "IVERILOG in DELLA node"
+    ICARUS_HOME=/tigress/jbalkind/utils
+fi
+echo "ICARUS_HOME=${ICARUS_HOME}"
