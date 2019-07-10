1eeb9d2664d007692885e5b817580afc2e036f35
Georgios <gt5@princeton.edu>
2019-07-10 17:13:19 -0400

    update tursi dynamic node script

--- tursi_dynamic_node.sh
+++ tursi_dynamic_node.sh
@@ -14,102 +14,34 @@ PICKLE_OUT_DIR=./pickles
 mkdir -p ${PICKLE_OUT_DIR}
 
 
+# Setup Dynamic Node example
+CORE="openpiton::dynamic_node"
+NETWORK_CONFIG="2dmesh"
+V_NAMING='NETWORK_${NETWORK_CONFIG}/'
 
-function tursi_all() {
-    echo "tursi_all()"
-    for _X_TILE in "${X_TILES[@]}"; do
-        for _Y_TILE in "${Y_TILES[@]}"; do
-            for _NETWORK_CONFIG in "${NETWORK_CONFIGS[@]}"; do
-                for _L1I_SIZE in "${L1I_SIZES[@]}"; do
-                    for _L1D_SIZE in "${L1D_SIZES[@]}"; do
-                        for _L15_SIZE in "${L15_SIZES[@]}"; do
-                            for _L2_SIZE in "${L2_SIZES[@]}"; do
-                                for _L1I_ASSOC in "${L1I_ASSOCS[@]}"; do
-                                    for _L1D_ASSOC in "${L1D_ASSOCS[@]}"; do
-                                        for _L15_ASSOC in "${L15_ASSOCS[@]}"; do
-                                            for _L2_ASSOC in "${L2_ASSOCS[@]}"; do
-                                                for CORE in "${CORES[@]}"; do
-                                                    VALID_CONF=$((
-                                                            (${_L1I_SIZE} <= ${_L2_SIZE}) &&
-                                                            (${_L1D_SIZE} <= ${_L15_SIZE}) &&
-                                                            (${_L15_SIZE} <= ${_L2_SIZE})))
-                                                    INVALID_ASSOC=$((
-                                                            (
-                                                                (${_L1I_SIZE}  == 8192) &&
-                                                                (${_L1I_ASSOC} != 2)) ||
-                                                            (
-                                                                (${_L1D_SIZE} == 4096) &&
-                                                                (${_L1D_ASSOC} != 2))))
-                                                    if [ ${INVALID_ASSOC} == "1" ]; then
-                                                        echo "INVALID ASSOC: ${PICKLE_OUT_DIR}/${CORE}_-_${_X_TILE}x${_Y_TILE}_${_NETWORK_CONFIG}_${_L1I_SIZE}-${_L1D_SIZE}-${_L15_SIZE}-${_L2_SIZE}_${_L1I_ASSOC}-${_L1D_ASSOC}-${_L15_ASSOC}-${_L2_ASSOC}.pickle.v"
-                                                        VALID_CONF=0
-                                                    fi
-                                                    
-                                                    if [ ${VALID_CONF} == "1" ]; then
-                                                        tursi --pickle --fusesoc_core ${CORE} \
-                                                            --network_config  ${_NETWORK_CONFIG} \
-                                                            --x_tiles ${_X_TILE} \
-                                                            --y_tiles ${_Y_TILE} \
-                                                            --config_l1i_size ${_L1I_SIZE} \
-                                                            --config_l1d_size ${_L1D_SIZE} \
-                                                            --config_l15_size ${_L15_SIZE} \
-                                                            --config_l2_size ${_L2_SIZE} \
-                                                            --config_l1i_associativity ${_L1I_ASSOC} \
-                                                            --config_l1d_associativity ${_L1D_ASSOC}  \
-                                                            --config_l15_associativity ${_L15_ASSOC} \
-                                                        --config_l2_associativity  ${_L2_ASSOC}
-                                                        CORE_UNDERSCORE=$(echo ${CORE} | tr ':' '_')
-                                                        MODULE_NAME=$(echo ${CORE} | cut -d ':' -f 3)
-                                                        PICKLE_OUT=build/build/${CORE_UNDERSCORE}_0.1/pickle-icarus/${CORE_UNDERSCORE}_0.1
- 
-                                                        # Create folder
-                                                        PICKLE_FOLDER="${PICKLE_OUT_DIR}/${MODULE_NAME}/`eval echo ${V_NAMING}`"
-                                                        mkdir -p ${PICKLE_FOLDER}
-                                                        echo ${PICKLE_FOLDER}
 
-                                                        # Move and rename pickled file
-                                                        PICKLE_RENAMED_OUT="${PICKLE_FOLDER}/${MODULE_NAME}.pickle.v"
-                                                        mv ${PICKLE_OUT} ${PICKLE_RENAMED_OUT}
 
-                                                        # Move json file
-                                                        JSON_F=build/build/floorplan.json
-                                                        mv ${JSON_F} "${PICKLE_FOLDER}/"
+set -x
+tursi --pickle --fusesoc_core ${CORE} --network_config "2dmesh"
+set +x
 
-                                                        echo ${PICKLE_RENAMED_OUT}
-                                                        exit
-                                                    fi
-                                                        
-                                                done
-                                            done
-                                        done
-                                    done
-                                done
-                            done
-                        done
-                    done
-                done
-            done
-        done
-    done   
-}
+# Copy pickle filed to correct location
+CORE_UNDERSCORE=$(echo ${CORE} | tr ':' '_')
+MODULE_NAME=$(echo ${CORE} | cut -d ':' -f 3)
+PICKLE_OUT=build/build/${CORE_UNDERSCORE}_0.1/pickle-icarus/${CORE_UNDERSCORE}_0.1
 
-CORES=(
-"openpiton::dynamic_node"
-)
+# Create folder
+PICKLE_FOLDER="${PICKLE_OUT_DIR}/${MODULE_NAME}/`eval echo ${V_NAMING}`"
+mkdir -p ${PICKLE_FOLDER}
+#echo ${PICKLE_FOLDER}
 
-X_TILES=(1)
-Y_TILES=(1)
-NETWORK_CONFIGS=("2dmesh")
+# Move and rename pickled file
+PICKLE_RENAMED_OUT="${PICKLE_FOLDER}/${MODULE_NAME}.pickle.v"
+mv ${PICKLE_OUT} ${PICKLE_RENAMED_OUT}
 
-L1I_SIZES=(8192) # 8K   16K 32K
-L1D_SIZES=(4096)  # 4K    8K 16K
-L15_SIZES=(16384) # 8K   16K 32K
-L2_SIZES=(65536)      # 64K 128K
+# Move json file
+JSON_F=build/build/floorplan.json
+mv ${JSON_F} "${PICKLE_FOLDER}/"
 
-L1I_ASSOCS=(2) # if smallest 2 otherwise 4
-L1D_ASSOCS=(2)
-L15_ASSOCS=(4)
-L2_ASSOCS=(4)
-
-V_NAMING='NETWORK_${_NETWORK_CONFIG}/'
-tursi_all;
+#echo ${PICKLE_RENAMED_OUT}
+echo "${PICKLE_RENAMED_OUT}"
